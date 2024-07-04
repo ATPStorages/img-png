2b461208ea2655586a687981c185729687cf19c4
ATPStorages <pmt.mailservice@gmail.com>
2024-07-04 17:50:04 -0500

    Update tIME to specification defined constraints
    New Data_Definition naming

--- src/time.ads
+++ src/time.ads
@@ -1,11 +1,10 @@
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
 with Interfaces; use Interfaces;
 with PNG;
 
 package tIME is
-   
-   type Chunk_Data_Info is new PNG.Chunk_Data_Info with record
+
+   type Data_Definition is new PNG.Base_Chunk_Data_Definition with record
       Year   : Unsigned_16;
       Month  : Unsigned_8 range 1 .. 12;
       Day    : Unsigned_8 range 1 .. 31;
@@ -13,10 +12,10 @@ package tIME is
       Minute : Unsigned_8 range 0 .. 59;
       Second : Unsigned_8 range 0 .. 60;
    end record;
-   
-   overriding procedure Decode (Self : in out Chunk_Data_Info; 
+
+   overriding procedure Decode (Self : in out Data_Definition;
                                 S : Stream_Access;
-                                C : PNG.Chunk; 
+                                C : PNG.Chunk;
                                 V : PNG.Chunk_Vectors.Vector;
                                 F : File_Type);
 
