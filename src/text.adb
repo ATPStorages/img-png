1968761cc6183bee2d233b240edf042c46530d64
ATPStorages <pmt.mailservice@gmail.com>
2024-07-04 21:06:24 -0500

    Use TypeRaw for static-ness

--- src/text.adb
+++ src/text.adb
@@ -1,28 +1,17 @@
-with Ada.Containers; use Ada.Containers;
-with Ada.Text_IO;
-with Ada.Characters.Latin_1;
-with PNG; use PNG;
-
 package body tEXt is
 
-   overriding procedure Decode (Self : in out Chunk_Data_Info; S : Stream_Access; C : PNG.Chunk; V : PNG.Chunk_Vectors.Vector; F : File_Type) is
+   overriding procedure Decode (Self : in out Data_Definition;
+                                S : Stream_Access;
+                                C : PNG.Chunk;
+                                V : PNG.Chunk_Vectors.Vector;
+                                F : File_Type)
+   is
+      Offset : Natural;
    begin
-      --  if C.ChunkSize /= 13 then
-      --     raise PNG.BAD_CHUNK_SIZE_ERROR with "IHDR size of (" & C.ChunkSize'Image & " ) bytes incorrect, should be 13";
-      --  elsif V.Length > 0 then
-      --     raise PNG.DUPLICATE_CHUNK_ERROR with "A valid PNG stream must contain only 1 IHDR chunk";
-      --  end if;
-
-      Self.Keyword := PNG.Decode_Null_String (S);
-      declare
-         Text : String (1 .. Natural (C.Length) - Length (Self.Keyword) - 1);
-      begin
-         String'Read (S, Text);
-         Append (Self.Text, Text);
-      end;
-      
-      Ada.Text_IO.Put_Line ("      tEXt Keyword : " & To_String (Self.Keyword));
-      Ada.Text_IO.Put_Line ("      tEXt String  : " & To_String (Self.Text));
+      Self.Keyword := PNG.Decode_Null_String (S, Offset);
+      Self.Text    := To_Unbounded_String (PNG.Decode_String_Chunk_End (S, F,
+                                           Natural (C.Length),
+                                           Offset));
    end Decode;
 
 end tEXt;
--- src/text.ads
+++ src/text.ads
@@ -1,15 +1,20 @@
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
-with Interfaces; use Interfaces;
 with PNG;
 
 package tEXt is
-   
-   type Chunk_Data_Info is new PNG.Chunk_Data_Info with record
+
+   TypeRaw : constant PNG.Chunk_Type := 16#74455874#;
+
+   type Data_Definition is new PNG.Base_Chunk_Data_Definition with record
       Keyword : Unbounded_String;
       Text    : Unbounded_String;
    end record;
-   
-   overriding procedure Decode (Self : in out Chunk_Data_Info; S : Stream_Access; C : PNG.Chunk; V : PNG.Chunk_Vectors.Vector; F : File_Type);
+
+   overriding procedure Decode (Self : in out Data_Definition;
+                                S : Stream_Access;
+                                C : PNG.Chunk;
+                                V : PNG.Chunk_Vectors.Vector;
+                                F : File_Type);
 
 end tEXt;
