1925f25bbdf9403418f24ceea764ec904ad95c97
ATPStorages <pmt.mailservice@gmail.com>
2024-07-04 21:14:49 -0500

    Use TypeRaw for static-ness
    Add IDAT constraints as defined by specification

--- src/idat.adb
+++ src/idat.adb
@@ -1,19 +1,23 @@
-with Ada.Containers; use Ada.Containers;
-with Ada.Text_IO; use Ada.Text_IO;
+with Interfaces; use Interfaces;
 
 package body IDAT is
 
-   overriding procedure Decode (Self : in out Chunk_Data_Info; S : Stream_Access; C : PNG.Chunk; V : PNG.Chunk_Vectors.Vector; F : Ada.Streams.Stream_IO.File_Type) is
+   overriding procedure Decode (Self : in out Data_Definition;
+                                S : Stream_Access;
+                                C : PNG.Chunk;
+                                V : PNG.Chunk_Vectors.Vector;
+                                F : File_Type)
+   is
    begin
-      --  if C.ChunkSize /= 13 then
-      --     raise PNG.BAD_CHUNK_SIZE_ERROR with "IHDR size of (" & C.ChunkSize'Image & " ) bytes incorrect, should be 13";
-      --  elsif V.Length > 0 then
-      --     raise PNG.DUPLICATE_CHUNK_ERROR with "A valid PNG stream must contain only 1 IHDR chunk";
-      --  end if;
-      
-      Chunk_Data_Info'Read (S, Self);
-      
-      Put_Line ("      IDAT Image Data Size :" & Self.FrameData'Size'Image);
+      if
+        V.Last_Element.TypeInfo.Raw /= TypeRaw and then
+        PNG.Chunk_Count (V, TypeRaw) > 0
+      then
+         raise PNG.BAD_STRUCTURE_ERROR
+           with "IDAT chunks should be consective in a valid PNG datastream";
+      end if;
+
+      Data_Definition'Read (S, Self);
    end Decode;
 
 end IDAT;
--- src/idat.ads
+++ src/idat.ads
@@ -1,13 +1,19 @@
 with Ada.Streams.Stream_IO; use Ada.Streams.Stream_IO;
-with Interfaces; use Interfaces;
 with PNG;
 
 package IDAT is
 
-   type Chunk_Data_Info (DataLength: PNG.Unsigned_31) is new PNG.Chunk_Data_Info with record
-      FrameData     : PNG.Chunk_Data_Array (1 .. DataLength);
+   TypeRaw : constant PNG.Chunk_Type := 16#49444154#;
+
+   type Data_Definition (DataLength : PNG.Unsigned_31) is
+     new PNG.Base_Chunk_Data_Definition with record
+      FrameData : PNG.Chunk_Data_Array (1 .. DataLength);
    end record;
-   
-   overriding procedure Decode (Self : in out Chunk_Data_Info; S : Stream_Access; C : PNG.Chunk; V : PNG.Chunk_Vectors.Vector; F : Ada.Streams.Stream_IO.File_Type);
+
+   overriding procedure Decode (Self : in out Data_Definition;
+                                S : Stream_Access;
+                                C : PNG.Chunk;
+                                V : PNG.Chunk_Vectors.Vector;
+                                F : File_Type);
 
 end IDAT;
